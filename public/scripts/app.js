9be365d6bfcd78c1d5e9f0c032815943407ff41a
lauratan <laura.tan108@gmail.com>
2018-06-22 01:37:44 +0000

    added momentjs for time stamp on new tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,69 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// Test / driver code (temporary). Eventually will get this from the server.
-// const data = [
-//     {  
-//       "user": {
-//         "name": "Doggo",
-//         "avatars": {
-//           "small":   "https://www.guidedogs.org/wp-content/uploads/2018/01/Mobile.jpg"
-//         },
-//         "handle": "@dog"
-//       },
-//       "content": {
-//         "text": "woof"
-//       },
-//       "created_at": 146111623222
-//     },
-//     { 
-//       "user": {
-//         "name": "Newton",
-//         "avatars": {
-//           "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//           "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//           "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//         },
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1461116232227
-//     },
-//     {
-//       "user": {
-//         "name": "Descartes",
-//         "avatars": {
-//           "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//           "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//           "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//         },
-//         "handle": "@rd" },
-//       "content": {
-//         "text": "Je pense , donc je suis"
-//       },
-//       "created_at": 1461113959088
-//     },
-//     {
-//       "user": {
-//         "name": "Johann von Goethe",
-//         "avatars": {
-//           "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//           "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//           "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//         },
-//         "handle": "@johann49"
-//       },
-//       "content": {
-//         "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//       },
-//       "created_at": 1461113796368
-//     }
-//   ]
+//Client-side JS logic goes here
 
 $(document).ready(function(){
 
@@ -79,7 +14,7 @@ $(document).ready(function(){
               avatar: tweet.user.avatars.small,
               handle: tweet.user.handle,
               content: tweet.content.text,
-              time: Math.round(tweet.created_at / 86400000000)
+              time: moment(tweet.created_at).startOf('minute').fromNow() //Math.round(tweet.created_at / 86400000000)
               // try using momentjs
           }
           let result = createTweetElement(newObj);
@@ -104,7 +39,7 @@ $(document).ready(function(){
             <h4> ${escape(tweeterObj.handle)}</h4>
           </header>
           <p> ${escape(tweeterObj.content)} </p> 
-          <footer> ${escape(tweeterObj.time)} days ago
+          <footer> ${escape(tweeterObj.time)}
               <span class="footerIcons">
                 <i class="fa fa-flag"></i>
                 <i class="fa fa-retweet"></i>
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -4,7 +4,6 @@ const userHelper    = require("../lib/util/user-helper")
 
 const express       = require('express');
 const tweetsRoutes  = express.Router();
-// const moment        = require('moment');
 
 module.exports = function(DataHelpers) {
 
